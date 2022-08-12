07969bd3e9abc3b323b7822f619e795adc637094
Ratnopam Chakrabarti <ratnopamc@yahoo.com>
2022-08-12 08:11:01 -0700

    Release v3.0.0 of product api

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ app.use(AWSXRay.express.openSegment('Product-Detail-V1'));
 app.get("/catalogDetail", (req, res, next) => {
   console.log("Catalog Detail Version 1 Get Request Successful");
   res.json({
-             "version":"2",
-             "vendors":[ "ABC.com", "XYZ.com", "PQR.com", "Norway.com"]
+             "version":"3",
+             "vendors":[ "ABC.com", "XYZ.com", "PQR.com", "Norway.com", "Spain.com"]
               } )
 });
 
